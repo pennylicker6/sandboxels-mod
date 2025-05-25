be9a453238fb3514d9ca938fa88341450161dc61
pennylicker6 <yourshirtisonbackwards@gmail.com>
2025-05-24 18:34:12 -0700

    Add files via upload

---
+++ perfect_coolant.js
@@ -0,0 +1,15 @@
+elements.perfect_coolant = {
+    color: "#00e0ff",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    state: "liquid",
+    density: 1000,
+    temperature: -100,
+    tempHigh: Infinity,
+    tempLow: -273.15,
+    conduct: 1,
+    hardness: 1,
+    noHeatTransfer: true,
+    hidden: false,
+    desc: "The ultimate coolant. Always -100°C, never evaporates or absorbs heat."
+};
